f538ae40ffb84fdd50f824ecabcea8cd7d64e247
Henry Smith <work.henrysmith@gmail.com>
2022-09-14 20:34:54 +0700

    create a very first home page for pharmacity

---
+++ src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => (
+    <React.Fragment>
+        <h1>Hello World, Welcome to Pharmacity</h1>
+        <p>Mục tiêu của Pharmacity trong 5 năm là đưa quy mô chuỗi nhà thuốc lớn nhất Việt Nam hiện nay tăng lên 5.000 nhà thuốc, song song đó là phát triển một hệ sinh thái chăm sóc sức khỏe toàn diện dành cho người dân.</p>
+        <p>Pharmacity cũng có kế hoạch củng cố cơ sở hạ tầng kỹ thuật số của mình. Theo kế hoạch 05 năm đến 2025, Pharmacity sẽ ra mắt một siêu ứng dụng - super app nhằm mang lại nhiều dịch vụ khác nhau, bao gồm Dược sĩ và Bác sĩ trực tuyến, đặt xe cấp cứu và các dịch vụ chăm sóc sức khỏe tại nhà cũng như dịch vụ chăm sóc người bệnh.</p>
+    </React.Fragment>    
+);
+
+export default HomePage;
