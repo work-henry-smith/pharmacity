cca28b29cad10e1999708fd2ac35f988dd589394
Henry Smith <work.henrysmith@gmail.com>
2022-09-15 08:09:41 +0700

    debug the semantic error of article-one

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from './pages/Homepage';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
-import ArticlePage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
 
 function App() {
   return (
@@ -11,9 +11,9 @@ function App() {
       <div className="App">
         <Routes>
           <Route index element={<HomePage />} />
-          <Route path='/about' element={<AboutPage />} />
-          <Route path='/articles-list' element={<ArticlesList />} />
-          <Route path='/article-one' element={<ArticlePage />} />
+          <Route path='about' element={<AboutPage />} />
+          <Route path='articles-list' element={<ArticlesList />} />
+          <Route path='article-one' element={<ArticlePage />} />
         </Routes>
       </div>
     </Router>
