b536bb60e35f16a025ef9ac3be28e3bc754e9ab4
Henry Smith <work.henrysmith@gmail.com>
2022-09-15 17:00:44 +0700

    create a new content link to article page link

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-    <>
-        <h1>Article 01</h1>
-        <h3>Công ty địa phương - Nguồn đầu tư toàn cầu</h3>
-        <p>Pharmacity có sự hỗ trợ tài chính rất mạnh mẽ từ trong nước cho đến quốc tế, bao gồm cả công ty cổ phần Mekong Capital có trụ sở tại Việt Nam và công ty đầu tư cổ phần tư nhân TR Capital có trụ sở tại Hong Kong. Ngành công nghiệp dược phẩm tại Việt Nam hiện tại trị giá 7.4 tỷ USD. Chi tiêu trung bình cho dược phẩm đã tăng với tốc độ trung bình 14% trong 10 năm qua và dự kiến sẽ tiếp tục tăng trong tương lai.</p>
-    </>    
-);
+const ArticlePage = ({match}) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article === name);
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph,key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+        </>
+    );
+};
 
 export default ArticlePage;
