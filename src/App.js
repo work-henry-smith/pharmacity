e13f5d9c8b1894b3f5f032bfa9367070de83b9c3
Henry Smith <work.henrysmith@gmail.com>
2022-09-15 07:51:24 +0700

    add 3 page-components into main app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import HomePage from './pages/Homepage';
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/AboutPage';
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <div className="App">
         <Routes>
           <Route index element={<HomePage />} />
+          <Route path='/about' element={<AboutPage />} />
+          <Route path='/articles-list' element={<ArticlesList />} />
+          <Route path='/article-one' element={<ArticlePage />} />
         </Routes>
       </div>
     </Router>
