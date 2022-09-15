bd92a8c6b4ab54d95bdd0bb3567e6d07912b86b9
Henry Smith <work.henrysmith@gmail.com>
2022-09-15 20:44:09 +0700

    get stuck with url parameters !

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import * as React from 'react';
+import { useParams } from 'react-router-dom';
 import articleContent from './article-content';
 
-const ArticlePage = ({match}) => {
-    const name = match.params.name;
-    const article = articleContent.find(article => article === name);
+const ArticlePage = () => {
+    const [news, setNews] = React.useState(null);
+    const {name} = useParams();
+
+    React.useEffect(() => {articleContent(name).then(setNews)},[name]);
+
     return (
         <>
-            <h1>{article.title}</h1>
-            {article.content.map((paragraph,key) => (
-                <p key={key}>{paragraph}</p>
-            ))}
+            <h1>{news.title}</h1>
+            {news.content.map((paragraph,key) => (<p key={key}>{paragraph}</p>))}
         </>
     );
 };
