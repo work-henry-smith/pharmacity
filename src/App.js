0170a54d71235f8af817c0cbd93014a7cda8d98b
Henry Smith <work.henrysmith@gmail.com>
2022-09-15 16:21:34 +0700

    create the nav bar coresponding with url

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavBar />
         <div id='page-body'>
           <Routes>
             <Route index element={<HomePage />} />
-            <Route path='about' element={<AboutPage />} />
-            <Route path='articles-list' element={<ArticlesList />} />
-            <Route path='article-one' element={<ArticlePage />} />
+            <Route path='/about' element={<AboutPage />} />
+            <Route path='/articles-list' element={<ArticlesList />} />
+            <Route path='/article/:name' element={<ArticlePage />} />
           </Routes>
         </div>
       </div>
